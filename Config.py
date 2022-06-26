af404b1989b80e159481a2231600fe90cefeeb96
Hilocv <101907782+Hilocv@users.noreply.github.com>
2022-06-26 10:09:32 -0400

    Update Config.py

--- Config.py
+++ Config.py
@@ -11,6 +11,6 @@ if ENVIRONMENT:
     BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
 else:
     # Fill the Values
-    API_ID = 0
-    API_HASH = ""
-    BOT_TOKEN = ""
+    API_ID = 14681595
+    API_HASH = "a86730aab5c59953c424abb4396d32d5"
+    BOT_TOKEN = "5362722840:AAGM04PCx8pQNdGKGPqGFBvLxjW36AO8dpo"
